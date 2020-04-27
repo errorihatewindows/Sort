2b099006d8a08ce76008e38ae62bc3d01404ab64
Markus Liebscher <markusliebscher2@gmail.com>
2020-04-27 18:38:49 +0200

    Draw_Line()

--- Sort/Form1.Designer.cs
+++ Sort/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackColor = System.Drawing.SystemColors.MenuText;
             this.ClientSize = new System.Drawing.Size(800, 798);
             this.Name = "Form1";
             this.Text = "Form1";
--- Sort/Form1.cs
+++ Sort/Form1.cs
@@ -23,16 +23,23 @@ namespace Sort
             sorting.generateArray(800);
         }
 
-        public void DrawArray(int[] array, Graphics e)
+        public void DrawArray(Graphics e)
         {
-            for (int i = 0; i < array.Length; i++)
-                e.DrawLine(Pens.White, i, (Height - 37), i, (Height - 37) - array[i]);
+            for (int i = 0; i < sorting.get_Array().Length; i++) 
+                e.DrawLine(Pens.White, i, (Height - 37), i, (Height - 37) - sorting.get_Array()[i]);
+        }
 
+        public void Draw_Line(int x, int y, Graphics e)
+        {
+            //BlackBackground
+            e.DrawLine(Pens.Black, x, 0, x, (Height + 37));
+            //Line
+            e.DrawLine(Pens.White, x, (Height - 37), x, (Height - 37) - y);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            DrawArray(sorting.get_Array(), e.Graphics);
+            DrawArray(e.Graphics);
         }
 
         private void Form1_Shown(object sender, EventArgs e)
