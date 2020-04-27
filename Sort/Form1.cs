de0f0a8b86ad570b2666cec85d4012d58aa69a53
Markus Liebscher <markusliebscher2@gmail.com>
2020-04-27 15:56:45 +0200

    Draw Array

--- Sort/Form1.cs
+++ Sort/Form1.cs
@@ -12,9 +12,29 @@ namespace Sort
 {
     public partial class Form1 : Form
     {
+        int[] testarray = new int[800];
+
         public Form1()
         {
             InitializeComponent();
+            DoubleBuffered = true;
+
+            Random zufall = new Random();
+
+            for (int i = 0; i < testarray.Length; i++)
+                testarray[i] = zufall.Next(1, 801);
+
+        }
+
+        public void DrawArray(int[] array, Graphics e)
+        {
+            for(int i = 0; i < array.Length; i++)
+                e.DrawLine(Pens.Black, i, Height, i, Height - array[i]);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawArray(testarray, e.Graphics);
         }
     }
 }
